a3b05d0ac2349a14e63d321cec8e9f9be521b457
wellwind <wellwind@mail2000.com.tw>
2016-05-20 16:54:41 +0800

    hello world

---
+++ HelloWorld.ts
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public static sayHi(): number{
+        console.log("Hello World!");
+        return 1;
+    }
+}
+
+HelloWorld.sayHi();
---
+++ scripts/HelloWorld.js
@@ -0,0 +1,11 @@
+var HelloWorld = (function () {
+    function HelloWorld() {
+    }
+    HelloWorld.sayHi = function () {
+        console.log("Hello World!");
+        return 1;
+    };
+    return HelloWorld;
+}());
+HelloWorld.sayHi();
+//# sourceMappingURL=HelloWorld.js.map
