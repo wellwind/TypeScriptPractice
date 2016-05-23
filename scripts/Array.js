f464de0eff8dbc58e735ac6c9f3125fa37157d9c
wellwind <wellwind@mail2000.com.tw>
2016-05-23 10:51:40 +0800

    Array practice

---
+++ scripts/Array.js
@@ -0,0 +1,9 @@
+var idList1 = [1, 2, 3];
+var idList2 = [3, 2, 1];
+idList1.forEach(function (id) {
+    console.log(id);
+});
+idList2.forEach(function (id) {
+    console.log(id);
+});
+//# sourceMappingURL=Array.js.map
---
+++ src/Array.ts
@@ -0,0 +1,10 @@
+var idList1: number[] = [1, 2, 3];
+var idList2: Array<number> = [3, 2, 1];
+
+idList1.forEach(function(id){
+    console.log(id);
+});
+
+idList2.forEach(function(id){
+    console.log(id);
+});
