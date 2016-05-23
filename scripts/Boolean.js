8da9ec961b19f558bcaeaa71611dbc8bb2de5f49
wellwind <wellwind@mail2000.com.tw>
2016-05-23 10:04:14 +0800

    boolean practice

---
+++ scripts/Boolean.js
@@ -0,0 +1,8 @@
+var isReady = true;
+if (isReady) {
+    console.log("I'm ready.");
+}
+else {
+    console.log("I'm not ready.");
+}
+//# sourceMappingURL=Boolean.js.map
---
+++ src/Boolean.ts
@@ -0,0 +1,7 @@
+var isReady: boolean = true;
+
+if(isReady){
+    console.log("I'm ready.");
+}else{
+    console.log("I'm not ready.");  
+}
